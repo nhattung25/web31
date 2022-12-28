f678211f4e3a4e3d433a1e4443a3cc3882cc9cda
nhattung25 <117737524+nhattung25@users.noreply.github.com>
2022-12-28 12:20:47 +0700

    update

---
+++ day20/btvn/b4 Thực hành card/index.js
@@ -0,0 +1,6 @@
+let ul = document.querySelector("#pro-plan ul");
+
+let newOtp = document.createElement("li");
+newOtp.innerText = "24/7 Phone support";
+console.log(newOtp);
+ul.appendChild(newOtp);
