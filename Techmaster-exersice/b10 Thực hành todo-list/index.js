481e55b9b0662010ab919b6bfa2b5b6f7e252bc4
nhattung <nhattung725@gmail.com>
2023-01-05 20:40:15 +0700

    update

--- Techmaster-exersice/b10 Thực hành todo-list/index.js
+++ Techmaster-exersice/b10 Thực hành todo-list/index.js
@@ -1,3 +1,12 @@
+let isUpdate = false;
+let idUpdate = null;
+let todo_list = document.querySelector(".list");
+let addBtn = document.querySelector(".input");
+let input_tex = document.querySelector("#input-tex");
+let allOpt = document.querySelector("#all");
+let pendingOtp = document.querySelector("#pending");
+let doneOtp = document.querySelector("#done");
+
 function createId() {
   let id = Math.floor(Math.random() * 10000);
   return id;
@@ -20,7 +29,6 @@ let todos = [
   },
 ];
 
-let todo_list = document.querySelector(".list");
 function renderUI(arr) {
   todo_list.innerHTML = "";
   if (arr.length === 0) {
@@ -33,11 +41,11 @@ function renderUI(arr) {
           <div class="todo-item">
             <input type="checkbox" id="checkbox" ${
               t.status ? "checked" : ""
-            } onClick="doneStatus(${t.id})"/>
+            } onClick="doneStatus(${t.id})"/> 
             <div class="checkbox-title">${t.title}</div>
           </div>
           <div class="option">
-            <button class="btn btn-update" >
+            <button class="btn btn-update" onClick="updateItem(${t.id})">
               <img
                 src="https://buihien0109.github.io/HTML5-Games/DOM-project/todo/img/pencil.svg"
                 alt="icon"
@@ -56,27 +64,33 @@ function renderUI(arr) {
 
 renderUI(todos);
 
-let addBtn = document.querySelector(".input");
-let input_tex = document.querySelector("#input-tex");
+addBtn.addEventListener("click", function () {
+  let input_content = input_tex.value;
+  if (input_content == "") {
+    alert("Không được để trống công việc");
+    return;
+  }
 
-function addTodo() {
-  addBtn.addEventListener("click", function () {
-    let input_content = input_tex.value;
-    if (input_content == "") {
-      alert("Không được để trống công việc");
-      return;
+  if (isUpdate) {
+    for (let i = 0; i < todos.length; i++) {
+      if (todos[i].id == idUpdate) {
+        todos[i].title = input_content;
+      }
     }
+    addBtn.textContent = "THÊM";
+    isUpdate = false;
+    idUpdate = null;
+  } else {
     let newTodo = {
       id: createId(),
       title: input_content,
       status: false,
     };
     todos.push(newTodo);
-    renderUI(todos);
-    input_tex.value = "";
-  });
-}
-addTodo();
+  }
+  renderUI(todos);
+  input_tex.value = "";
+});
 
 function deleteItem(id) {
   for (let i = 0; i < todos.length; i++) {
@@ -87,8 +101,6 @@ function deleteItem(id) {
   renderUI(todos);
 }
 
-deleteItem();
-
 function doneStatus(id) {
   for (let i = 0; i < todos.length; i++) {
     if (id == todos[i].id) {
@@ -98,4 +110,34 @@ function doneStatus(id) {
   renderUI(todos);
 }
 
-doneStatus();
+function updateItem(id) {
+  let title;
+  for (let i = 0; i < todos.length; i++) {
+    if (id == todos[i].id) {
+      title = todos[i].title;
+    }
+  }
+  addBtn.textContent = "CẬP NHẬT";
+  input_tex.value = title;
+  input_tex.focus();
+  idUpdate = id;
+  isUpdate = true;
+}
+let radio = document.getElementsByClassName("radio");
+function check() {
+  if (allOpt.checked) {
+    doneOtp.checked = false;
+    pendingOtp.checked = false;
+    renderUI(todos);
+  }
+  if (pendingOtp.checked) {
+    console.log("bbbb");
+    allOpt.checked = false;
+    doneOtp.checked = false;
+  }
+  if (doneOtp.checked) {
+    console.log("aaaa");
+    allOpt.checked = false;
+    pendingOtp.checked = false;
+  }
+}
